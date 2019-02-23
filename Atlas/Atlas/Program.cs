ffa17e9e24697c74c927db3ebcd6f50956bf702f
Robbie <robbiealternate@gmail.com>
2019-02-23 19:09:55 +0000

    Add ELO Defender version, sort all versions

--- Atlas/Atlas/Program.cs
+++ Atlas/Atlas/Program.cs
@@ -7,12 +7,14 @@ namespace RLBotCSharpExample
     {
         static void Main()
         {
+            Directory.SetCurrentDirectory(System.AppDomain.CurrentDomain.BaseDirectory);
+
             // Read the port from port.cfg.
             const string file = "port.cfg";
             string text = File.ReadAllLines(file)[0];
             int port = int.Parse(text);
 
-            // BotManager is a generic which takes in your bot as its T type.
+            // BotManager is a generic which takes in Atlas as its T-type.
             BotManager<Atlas> botManager = new BotManager<Atlas>();
 
             // Start the server on the port given in the port.cfg file.
---
+++ Versions/README.md
@@ -0,0 +1,4 @@
+# Atlas Releases - DD/MM/YYYY
+ELO Defender (23/2/2019)
+Christmas Invitational (21/12/2018)
+Winter Tournament Fix (15/12/2018)
